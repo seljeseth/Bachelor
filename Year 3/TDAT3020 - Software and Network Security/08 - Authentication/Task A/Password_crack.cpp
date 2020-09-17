e9a96d260f716809acd68b7f5f8bd203615b4e0c
Sabine Seljeseth <sabine.seljeseth@gmail.com>
2020-09-17 17:28:51 +0200

    Finished task a of assigment 8

--- Year 3/TDAT3020 - Software and Network Security/08 - Authentication/Task A/Password_crack.cpp
+++ Year 3/TDAT3020 - Software and Network Security/08 - Authentication/Task A/Password_crack.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <cstring>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -36,14 +37,10 @@ string pbkdf2(string input)
   return hex(key);
     
 }
-void crack(){
+string crack()
+{
+    //string key = pbkdf2("AA");
   string key = "ab29d7b5c589e18b52261ecba1d3a7e7cbf212c6";
-  /*char str[] = {'A','B','C','D','E','F','G','H',
-                'I','J','K','L','M','N','O','P',
-                'Q','R','S','T','U','V','W','X',
-                'Y','Z','a','b','c','d','e','f',
-                'g','h','i','j','k','l','m','n',
-                'o','p','q','r','s','t','u','v','w','x','y','z'};*/
   string chars[] = {"A", "B", "C", "D", "E", "F", "G",
                     "H", "I", "J", "K", "L", "M", "N",
                     "O", "P", "Q", "R", "S", "T", "U",
@@ -54,69 +51,37 @@ void crack(){
                     "u", "v", "w", "æ", "ø", "å", "x",
                     "y","z","0", "1", "2", "3", "4","5",
                      "6", "7", "8", "9"};
-  int length_array = sizeof(chars)/sizeof(chars[0]);
+
+  //vector<string> chars = {"A", "B"};
   string output = "";
-  for(int i = 0; i <2; i++)
-  {
-    string first_letter = chars[i];
+  vector<string> current_guesses;
+  vector<string> wrong_guesses = {output};
+  while(true){
+
+
+    for(string s: wrong_guesses)
     {
-      for (int j = 0; i < 2; j++)
+
+      for(string a : chars)
       {
-        string output = first_letter + chars[j];
-        cout << output << endl;
+        if(pbkdf2((s+a)) == key) return (s+a);
+        current_guesses.push_back((s + a));
+        
       }
       
+        
     }
-  }
+    wrong_guesses = current_guesses;
+    current_guesses = {};
+    cout << wrong_guesses[1].length() << endl;
+    
+    }
+    
 }
-
-  /*for(int i = 0; i < sizeof(str); i++){
-    string check;
-    check.push_back(str[i]);
-    if(pbkdf2((output+check)) == key){
-      cout<<check<<endl;
-    }else
-    {
-      
-    }*/
-    
-    
-  
-
-
-// Function to print permutations of string  
-// This function takes three parameters:  
-// 1. String  
-// 2. Starting index of the string  
-// 3. Ending index of the string.  
-/*void permute(string a, int l, int r)  
-{  
-    // Base case  
-    if (l == r)  
-        cout<<a<<endl;  
-    else
-    {  
-        // Permutations made  
-        for (int i = l; i <= r; i++)  
-        {  
-  
-            // Swapping done  
-            swap(a[l], a[i]);  
-  
-            // Recursion called  
-            permute(a, l+1, r);  
-  
-            //backtrack  
-            swap(a[l], a[i]);  
-        }  
-    }  
-}  */
-
-
 
 int main() {
  
 
- crack();
+ cout << crack() << endl;
 
 }
